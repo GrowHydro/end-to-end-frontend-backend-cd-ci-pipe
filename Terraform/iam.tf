52ae8973c870cda3533dc9f52bbbec91c9004a8f
williehydro <grow.hydros@gmail.com>
2023-07-06 07:14:58 -0700

    update

--- Terraform/iam.tf
+++ Terraform/iam.tf
@@ -16,24 +16,24 @@ resource "aws_iam_role" "pipe_role" {
   })
 
 }
-resource "aws_iam_role" "pipe_role_2" {
-  name = "${var.company}-pipe_role"
+# resource "aws_iam_role" "pipe_role_2" {
+#   name = "${var.company}-pipe_role"
 
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "iam.amazonaws.com"
-        }
-      }
-    ]
-  })
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "iam.amazonaws.com"
+#         }
+#       }
+#     ]
+#   })
 
-}
+# }
 
 data "aws_iam_policy_document" "pipeline-policy-document" {
   statement {
@@ -59,10 +59,10 @@ resource "aws_iam_policy" "pipeline-policy" {
   policy      = data.aws_iam_policy_document.pipeline-policy-document.json
 }
 
-resource "aws_iam_role_policy_attachment" "pipe-attachment-2" {
-  policy_arn = aws_iam_policy.pipeline-policy.arn
-  role       = aws_iam_role.pipe_role_2.id
-}
+# resource "aws_iam_role_policy_attachment" "pipe-attachment-2" {
+#   policy_arn = aws_iam_policy.pipeline-policy.arn
+#   role       = aws_iam_role.pipe_role_2.id
+# }
 
 resource "aws_iam_role_policy_attachment" "pipe-attachment" {
   policy_arn = aws_iam_policy.pipeline-policy.arn
@@ -70,7 +70,7 @@ resource "aws_iam_role_policy_attachment" "pipe-attachment" {
 }
 
 resource "aws_iam_role_policy_attachment" "pipe-fullaccess_attachment" {
-  policy_arn = "arn:aws:iam::aws:policy/IAMFullAccess"
+  policy_arn = "arn:aws:iam::aws:policy/PowerUserAccess"
   role       = aws_iam_role.pipe_role.id
 }
 
@@ -80,6 +80,7 @@ resource "aws_iam_role_policy_attachment" "pipe-fullaccess_attachment" {
 
 resource "aws_iam_role" "codebuild_role" {
   name = "${var.company}-codebuild-role"
+  
   assume_role_policy = jsonencode({
     Version = "2012-10-17"
     Statement = [
@@ -88,7 +89,7 @@ resource "aws_iam_role" "codebuild_role" {
         Effect = "Allow"
         Sid    = ""
         Principal = {
-          Service = "codepipeline.amazonaws.com"
+          Service = "codebuild.amazonaws.com"
         }
       },
     ]
@@ -139,24 +140,24 @@ resource "aws_iam_role_policy_attachment" "fullaccess_attachment" {
   role       = aws_iam_role.codebuild_role.id
 }
 
-resource "aws_iam_role_policy_attachment" "codebuild-attachment-iam" {
-  policy_arn = aws_iam_policy.codebuild-policy.arn
-  role       = aws_iam_role.codebuild_role_2.id
-}
+# resource "aws_iam_role_policy_attachment" "codebuild-attachment-iam" {
+#   policy_arn = aws_iam_policy.codebuild-policy.arn
+#   role       = aws_iam_role.codebuild_role_2.id
+# }
 
-resource "aws_iam_role" "codebuild_role_2" {
-  name = "${var.company}-codebuild-role"
-  assume_role_policy = jsonencode({
-    Version = "2012-10-17"
-    Statement = [
-      {
-        Action = "sts:AssumeRole"
-        Effect = "Allow"
-        Sid    = ""
-        Principal = {
-          Service = "iam.amazonaws.com"
-        }
-      },
-    ]
-  })
-}
+# resource "aws_iam_role" "codebuild_role_2" {
+#   name = "${var.company}-codebuild-role"
+#   assume_role_policy = jsonencode({
+#     Version = "2012-10-17"
+#     Statement = [
+#       {
+#         Action = "sts:AssumeRole"
+#         Effect = "Allow"
+#         Sid    = ""
+#         Principal = {
+#           Service = "iam.amazonaws.com"
+#         }
+#       },
+#     ]
+#   })
+# }
